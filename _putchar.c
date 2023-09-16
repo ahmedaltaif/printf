168f7753ad74ef3fb2d889c9ab176391f918e5ad
Namariq <nemonmari95@gmail.com>
2023-09-16 13:39:53 -0700

    refactor

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+
 /**
  * _putchar - function that print char.
  * @c: pointer to string.
  * Return: number of byets.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));

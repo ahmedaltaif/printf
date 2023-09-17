4809b2a5e54a71d5a48f8b6df1d321de565021d8
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-18 00:24:57 +0200

    commit

--- _printf.c
+++ _printf.c
@@ -4,48 +4,45 @@
  * @format: is a character string. The format string
  * Return: number of byets.
  */
-
-
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0;
-	unsigned int r;
+	unsigned int r = 0;
 	int numfb = 0;
 	va_list args;
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-
+		if (!format || (format[0] == '%' && !format[1]))
+			return (-1);
+		if (format[0] == '%' && format[1] == ' ' && !format[2])
+			return (-1);
 		if (format[i] != '%')
-		{
 			_putchar(format[i]);
-			numfb++;
-		}
-		else if ((format[i] == '%' && format[i + 1] == 'c'))
+		else if (format[i] == '%')
 		{
-			i++;
-			_putchar(va_arg(args, int));
-			numfb++;
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				i++;
+			}
+			else if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				r = _puts(va_arg(args, char *));
+				i++;
+				numfb = (numfb + (r - 1));
+			}
 		}
-		else if ((format[i] == '%' && format[i + 1] == 's'))
-		{
-			i++;
-			r = _puts(va_arg(args, char *));
-			numfb = (numfb + (r - 1));
-		}
-		else if ((format[i] == '%' && format[i + 1] == '%'))
-		{
-			_putchar('%');
-			i++;
-			numfb++;
-		}
+		numfb++;
 	}
-	va_end(args);
+	printf("no of byet: %d\n", numfb);
 	return (numfb);
+	va_end(args);
 }

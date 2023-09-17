ce06616fdf9d8e7f5b37931b85037ffb9295d35c
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-18 01:18:23 +0200

    commit

--- _printf.c
+++ _printf.c
@@ -6,34 +6,42 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0; unsigned int r = 0; int numfb = 0; va_list args;
+	unsigned int i = 0;
+	unsigned int r = 0;
+	int numfb = 0;
+	va_list args;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(args, format);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
 			_putchar(format[i]);
-			numfb++;
-		} else if (format[i] == '%')
+		else if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
 			{
 				_putchar('%');
 				i++;
-			} else if (format[i + 1] == 'c')
+			}
+			else if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(args, int));
 				i++;
-			} else if (format[i + 1] == 's')
+			}
+			else if (format[i + 1] == 's')
 			{
 				r = _puts(va_arg(args, char *));
 				i++;
 				numfb = (numfb + (r - 1));
-			} numfb++;
+			}
 		}
-	} return (numfb);
+		numfb++;
+	}
+	return (numfb);
 	va_end(args);
 }

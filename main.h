06faf57504220b58e4bf4b30a84ca4432187cf90
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-09-18 02:03:30 +0200

    commit

--- main.h
+++ main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include <unistd.h>
 int _printf(const char *format, ...);
 int _putchaarr(char c);

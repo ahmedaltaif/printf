f906a0c39dd08f36c4a9627c6b6a6a234c20beee
Namariq <nemonmari95@gmail.com>
2023-09-19 18:42:28 -0700

    message

--- main.h
+++ main.h
@@ -1,12 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+
 int _printf(const char *format, ...);
 int _putchaarr(char c);
 int _puttss(char *c);
 int _print_i(va_list i);
 int _printf_d(va_list args);
 int reverse_integer(int integer);
+
 #endif /* MAIN_H */
